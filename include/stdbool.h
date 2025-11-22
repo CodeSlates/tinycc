186dc93b6fc850c40748e9cd88db272eeab6ea6a
akire93597 <akire.93@hotmail.it>
2025-11-22 08:43:35 +0100

    Add stdbool stub
    Add a simple implementation of studbook.

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,11 +1,31 @@
 #ifndef _STDBOOL_H
 #define _STDBOOL_H
 
-/* ISOC99 boolean */
 
-#define bool	_Bool
-#define true	1
-#define false	0
+#ifdef __cplusplus
+
+/* C++ already has a native bool */
+#define _Bool bool
+#define bool bool
+#define true true
+#define false false
 #define __bool_true_false_are_defined 1
+
+#else /* __cplusplus */
+
+#ifndef __bool_true_false_are_defined
+
+#if !defined(__STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
+typedef unsigned char _Bool;
+#endif
+
+#define bool _Bool
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined 1
+
+#endif /* __bool_true_false_are_defined */
+
+#endif /* __cplusplus */
 
 #endif /* _STDBOOL_H */
